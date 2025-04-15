0d5430109734cfe7715128a122725be5d31b0cd1
Radiva279 <radivagalih12@gmail.com>
2025-04-15 11:11:07 +0700

    menampilkan dan mengisi array

--- array.cpp
+++ array.cpp
@@ -11,5 +11,14 @@ int skor[4]={9,13,20,11};
 string nama[3];
 
 int main(){
-    cout << "Isi data skor yang ketiga = " << skor[2] << endl;
+        cout << "Isi data skor yang ketiga = " << skor[2] << endl;
+        //Mengisi array pada index tertentu secara langsung
+        skor[2] = 50;
+        cout << "Isi data skor ketiga = " << skor[2] << endl;
+        
+        //Mengisi array dengan looping for
+        for(int i = 0; i < 3; i++){
+            cout << "Mahasiswa ke-" << i+1 << " = ";
+            cin >> nama[i];
+        }
 }
