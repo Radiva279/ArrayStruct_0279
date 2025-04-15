dfa09e13e75883734fc43176e1e7a9d04e8f585a
Radiva279 <radivagalih12@gmail.com>
2025-04-15 11:21:25 +0700

    menambahkan untuk menampilkan data dan menginput data

--- struct.cpp
+++ struct.cpp
@@ -14,4 +14,11 @@ int main(){
     mhs.nim = "20240140279";
     mhs.nama = "Radiva Galih";
     mhs.alamat = "Banguntapan"; 
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat = ";
+    cin >> mhs.alamat;
 }
